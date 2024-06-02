62e6051af56a309785e28af0bdff87d1482d7e9b
GraveRob <kazan154321@gmail.com>
2024-06-03 00:14:31 +0300

    feat: add new types of Typography component and add new props

--- mobile/src/components/General/Typography/index.tsx
+++ mobile/src/components/General/Typography/index.tsx
@@ -2,7 +2,14 @@ import { Text } from 'react-native';
 import { typeStyles, formatStyles } from './styles';
 import { TypographyProps } from './types';
 
-const Typography = ({ children, type, uppercase, italic }: TypographyProps) => {
+const Typography = ({
+  children,
+  type,
+  uppercase,
+  italic,
+  color,
+  capitalize,
+}: TypographyProps) => {
   const typeStyle = typeStyles[type];
 
   return (
@@ -11,6 +18,8 @@ const Typography = ({ children, type, uppercase, italic }: TypographyProps) => {
         typeStyle,
         uppercase && formatStyles.uppercase,
         italic && formatStyles.italic,
+        capitalize && formatStyles.capitalize,
+        !!color && { color },
       ]}>
       {children}
     </Text>
