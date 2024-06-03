115a7a1f844006962394d39277423b020f93fb88
GraveRob <kazan154321@gmail.com>
2024-06-03 23:38:31 +0300

    feat: update FloatingPanel to support several components

--- mobile/src/components/General/FloatingPanel/styles.ts
+++ mobile/src/components/General/FloatingPanel/styles.ts
@@ -2,7 +2,13 @@ import { StyleSheet } from 'react-native';
 import spacing from '../../../theme/spacing';
 
 const styles = StyleSheet.create({
-  container: { position: 'absolute', width: '100%', bottom: spacing(4) },
+  container: {
+    position: 'absolute',
+    width: '100%',
+    bottom: spacing(4),
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
 });
 
 export default styles;
