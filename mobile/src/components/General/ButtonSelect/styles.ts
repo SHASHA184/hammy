ecd6e133566b4bd9c75acea02f53f2a971a9cf35
GraveRob <kazan154321@gmail.com>
2024-06-03 23:36:28 +0300

    feat: add ButtonSelect component

---
+++ mobile/src/components/General/ButtonSelect/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '85%',
+  },
+});
+
+export default styles;
